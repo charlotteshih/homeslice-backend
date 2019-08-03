3ca95ecd1b0b583b40c5da197075b1fe9b62a963
Felix Meyer <phelixm@gmail.com>
2019-08-03 12:41:08 -0500

    start testing orders endpoint

---
+++ test/orders-endpoints.spec.js
@@ -0,0 +1,9 @@
+const knex = require("knex");
+const app = require("../src/app");
+const helpers = require("./test-helpers");
+
+describe("Orders Endpoints", function() {
+    let db
+
+    const {test}
+})
