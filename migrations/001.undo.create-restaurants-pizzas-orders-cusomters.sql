09afc6bdb5104aedb9d23098420413091c525a17
ryancahela <ryancahela@gmail.com>
2019-08-11 21:28:30 -0700

    fixed small bug in undo.create.types migrations

--- migrations/001.undo.create-restaurants-pizzas-orders-cusomters.sql
+++ migrations/001.undo.create-restaurants-pizzas-orders-cusomters.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS restaurants;
 DROP TABLE IF EXISTS pizzas;
 DROP TABLE IF EXISTS customers;
 
-DROP TYPE IF EXISTS size_options;
-DROP TYPE IF EXISTS pizza_types;
+DROP TYPE IF EXISTS pizza_size;
+DROP TYPE IF EXISTS pizza_type;
 DROP TYPE IF EXISTS order_status;
