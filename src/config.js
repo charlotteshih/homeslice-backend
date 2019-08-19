9fe2ac1ae175ce3e46cf5d23522f5e46863b1bc3
Charlotte Shih <shih.charlotte@gmail.com>
2019-08-18 18:12:10 -0700

    Setup for deployment

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DBASE_URL || 'postgresql://homeslice@localhost/homeslice',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://homeslice@localhost/homeslice',
+    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://homeslice@localhost/homeslice-test',
     JWT_SECRET: process.env.JWT_SECRET || 'gimme_that_good_za',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '24h'
 };
